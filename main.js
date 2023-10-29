03ac62e95d69cb1a707e6fee473a76f17a1be03d
Mike Rhodes <mike.rhodes@gmail.com>
2023-10-29 14:27:46 +0000

    feat: write up emit of arrays as couchdb-like map function in a test-ish harness

--- main.js
+++ main.js
@@ -12,52 +12,60 @@ doc = {
   "pets": ["alice", "bob", "eve"],
 };
 
-// paths returns an array of arrays of the ["key", "path", "value"]
-// for the value x passed in.
-function paths(x) {
-  if (x === null) {
-    return [[x]];
-  }
-  if (x.constructor === String) {
-    return [[x]];
-  }
-  if (x.constructor === Number) {
-    return [[x]];
-  }
-  if (x.constructor === Boolean) {
-    return [[x]];
-  }
-
-  if (x.constructor === Array) {
-    const r = [];
-    for (let i = 0; i < x.length; i++) {
-      const v = paths(x[i]);
-      v.forEach((subpath) => {
-        subpath.unshift(i);
-        r.push(subpath);
-      });
+function map(x) {
+  // paths returns an array of arrays of the ["key", "path", "value"]
+  // for the value x passed in.
+  paths = function (x) {
+    if (x === null) {
+      return [[x]];
     }
-    return r;
-  }
+    if (x.constructor === String) {
+      return [[x]];
+    }
+    if (x.constructor === Number) {
+      return [[x]];
+    }
+    if (x.constructor === Boolean) {
+      return [[x]];
+    }
 
-  if (x.constructor === Object) {
-    const r = [];
-    Object.keys(x).forEach((k) => {
-      const v = paths(x[k]);
-      v.forEach((subpath) => {
-        subpath.unshift(k);
-        r.push(subpath);
+    if (x.constructor === Array) {
+      const r = [];
+      for (let i = 0; i < x.length; i++) {
+        const v = paths(x[i]);
+        v.forEach((subpath) => {
+          subpath.unshift(i);
+          r.push(subpath);
+        });
+      }
+      return r;
+    }
+
+    if (x.constructor === Object) {
+      const r = [];
+      Object.keys(x).forEach((k) => {
+        const v = paths(x[k]);
+        v.forEach((subpath) => {
+          subpath.unshift(k);
+          r.push(subpath);
+        });
       });
-    });
-    return r;
-  }
+      return r;
+    }
+  };
+
+  const result = paths(x);
+  result.forEach((x) => {
+    emit(x, null);
+  });
 }
 
-// result = paths({ "a": 12, "b": "foo", "c": true });
-// result = paths({ "a": { "b": "foo" } });
-// result = paths({ "a": { "b": "foo", "c": true } });
-// result = paths({ "a": ["zero", 1, "foo", true, 4, "last"] });
-const result = paths(doc);
-result.forEach((x) => {
-  console.log("emit(", x, ", null)");
-});
+function emit(key, value) {
+  console.log("emit(", key, ", " + value + ")");
+}
+
+// map({ "a": 12, "b": "foo", "c": true });
+// map({ "a": { "b": "foo" } });
+// map({ "a": { "b": "foo", "c": true } });
+// map({ "a": ["zero", 1, "foo", true, 4, "last"] });
+map(doc);
