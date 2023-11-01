30d07f3e47ecb701277d0eb2736690250a81922e
Mike Rhodes <mike.rhodes@gmail.com>
2023-11-01 21:26:26 +0000

    Pull in PouchDB as demo vehicle
    PouchDB should be simpler to work with as it's all local, to work out
    simple things like how to AND efficiently, and OR, and work out the
    "basic operations" on the index, to encode into methods. But for now
    we just have a couple of trivial queries to start us on our way.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-doc = {
+const doc = {
   "_id": "234-23423-efsd-234",
   "person": {
     "age": 12,
@@ -16,7 +16,7 @@ doc = {
 function map(x) {
   // paths returns an array of arrays of the ["key", "path", "value"]
   // for the value x passed in.
-  paths = function (x) {
+  const paths = function (x) {
     if (x === null) {
       return [[x]];
     }
@@ -70,4 +70,72 @@ function emit(key, value) {
 // map({ "a": { "b": "foo" } });
 // map({ "a": { "b": "foo", "c": true } });
 // map({ "a": ["zero", 1, "foo", true, 4, "last"] });
-map(doc);
+// map(doc);
+
+var PouchDB = require("pouchdb");
+PouchDB.plugin(require("pouchdb-adapter-memory"));
+
+var db = new PouchDB("kittens", { adapter: "memory" });
+
+// document that tells PouchDB/CouchDB
+// to build up an index on doc.name
+var ddoc = {
+  _id: "_design/my_index",
+  views: {
+    gin: {
+      map: map.toString(),
+    },
+  },
+};
+// save it
+db.put(ddoc).then(function () {
+  // success!
+}).catch(function (err) {
+  // some error (maybe a 409, because it already exists?)
+});
+
+db.put(doc).catch((ex) => {
+  console.log(ex.status);
+});
+
+// Query for exact matches on the Rhodes family
+db.query("my_index/gin", {
+  key: ["person", "name", "second", "rhodes"],
+}).then(function (res) {
+  res.rows.forEach((row) => {
+    console.log(row);
+  });
+}).catch(function (err) {
+  // some error
+});
+
+db.post({ "foo": 12, "bar": "up" });
+db.post({ "foo": 22, "bar": "down" });
+db.post({ "foo": 32, "bar": "up" });
+db.post({ "foo": 42, "bar": "down" });
+db.post({ "foo": 42, "bar": "up" });
+
+// Query for foo = 22
+db.query("my_index/gin", {
+  key: ["foo", 22],
+}).then(function (res) {
+  res.rows.forEach((row) => {
+    console.log(row);
+  });
+}).catch(function (err) {
+  // some error
+});
+
+// Query for foo >= 22
+db.query("my_index/gin", {
+  startkey: ["foo", 22],
+  endkey: ["foo", {}],
+}).then(function (res) {
+  res.rows.forEach((row) => {
+    console.log(row);
+  });
+}).catch(function (err) {
+  // some error
+});
+
+// A simple AND foo >= 22 and bar = up
